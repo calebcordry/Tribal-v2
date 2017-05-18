810b6944d000167ef6b3102b49ba6eda80a86c94
Carlo Las Marias <carlol@gmail.com>
2017-05-18 11:35:40 -0700

    hero.js

---
+++ reactNative/src/Hero.js
@@ -0,0 +1,28 @@
+
+
+/* class Hero extends Component {
+  render() {
+    return (
+      <TouchableHighlight onPress={this.props._onPressButton} >
+        <View>
+          <Text style={styles.hero} >{this.props.name}</Text>
+          <Image source={this.props.image} style={styles.picture} />
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}*/
+
+
+// const team = [
+//   { name: 'Caleb', image: require('./public/images/caleb.jpg') },
+//   { name: 'Carlo', image: require('./public/images/carlo.jpg') },
+//   { name: 'Vaggelis', image: require('./public/images/vaggelis.jpg') }
+// ];
+
+
+{/*<View style={styles.profiles}>
+  {team.map((hero, index) => (
+    <Hero key={index} name={hero.name} image={hero.image} _onPressButton={() => this._onPressButton(hero.name)} />
+  ))}
+</View>*/}
